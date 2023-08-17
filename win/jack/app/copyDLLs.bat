d7b18eb24421a9473f95d468d04f6bc22cd4830b
Aaron <aaron@omniware.us>
2023-08-17 14:15:47 -0700

    Fixed windows build issue.

--- win/jack/app/copyDLLs.bat
+++ win/jack/app/copyDLLs.bat
@@ -1,8 +1,7 @@
 del ..\%3\*.dll /Q /F
-xcopy %1\dll\%2\%3\*.dll ..\%3 /F /Y /E /R
-xcopy *Msg.h %1\inc /F /Y /E /R
+xcopy %1\dll\%2\%3\*.dll ..\%3\*.dll /F /Y /E /R
 if "%3"=="Debug" (
-  xcopy %1\lib\%2\%3\*.pdb ..\%3 /F /Y /E /R
+  xcopy %1\lib\%2\%3\*.pdb ..\%3\*.pdb /F /Y /E /R
 )
 if "%4"=="Install" (
   %1\Install\installer.bat %1 %2 %3
