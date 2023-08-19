ddb52623ebad68a8976e8a7e7c95a84e8afbf742
Aaron <aaron@omniware.us>
2023-08-19 12:07:29 -0700

    Added flash erase batch file.

---
+++ utils/erase.bat
@@ -0,0 +1,5 @@
+cmd /c mb-objcopy -O srec ..\mb\arty_fw\Release\arty_fw.elf arty_fw.elf.srec
+cmd /c bootgen -arch fpga -image srecimage.bif -w -o srec.bin -interface spi
+cmd /c program_flash -f srec.bin -offset 0x600000 -flash_type s25fl128sxxxxxx0-spi-x1_x2_x4 -erase_only -url TCP:127.0.0.1:3121
+cmd /c bootgen -arch fpga -image bootimage.bif -w -o boot.bin -interface spi
+cmd /c program_flash -f boot.bin -offset 0x0 -flash_type s25fl128sxxxxxx0-spi-x1_x2_x4 -erase_only -url TCP:127.0.0.1:3121
